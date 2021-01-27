6fe2b80dd81a382517f35945a06a37e2dcaa1ecd
Tommy Harris <tobraha@tilde.team>
2021-01-26 21:18:33 -0700

    Initial Commit

---
+++ atg_run-CyLR.ps1
@@ -0,0 +1,41 @@
+#requires -RunAsAdministrator
+
+# Set variables
+Write-Host -NoNewline "`n [*] Setting up working folders, paths, and URLs...  "
+$date = Get-Date -Format "MM-dd-yyyy_HH-mm-ss"
+$workingDir = "$env:SYSTEMDRIVE\CyLR"
+$cylrExe = "$workingDir\CyLR.exe"
+$cylrOutputFileName = "CyLR-$env:COMPUTERNAME-$date.zip"
+$cylrUri = "http://download.ambitionsgroup.com/Software/CyLR-beta.exe"
+$netClient = New-Object System.Net.WebClient
+Write-Host "Done!"
+
+$zipPassword = ""
+$sftpServerIp = ""
+$sftpUserName = ""
+$sftpPassword = ""
+
+# Create working directory, download CyLR
+Write-Host -NoNewline " [*] Creating working directory and downloading tools...  "
+New-Item -ItemType Directory -Force -Path $workingDir | Out-Null
+Write-Host "Done!`n"
+
+Write-Host -NoNewline " --> Downloading CyLR executable...  "
+$netClient.DownloadFile($cylrUri, $cylrExe)
+Write-Host "Done!"
+
+# Run CyLR with default collection parameters; upload to ATG SFTP server
+Write-Host " --> Running CyLR with default collection parameters"
+Write-Host "  ########### BEGIN CYLR ###########`n"
+
+$cylrArgs = "-of", "$cylrOutputFileName", `
+            "-os", "/upload", `
+            "-zl", "7", `
+            "-zp", "$zipPassword", `
+            "-u", "$sftpUserName", `
+            "-p", "$sftpPassword", `
+            "-s", "$sftpServerIp"
+
+Start-Process -Wait -NoNewWindow -WorkingDirectory $workingDir "$cylrExe" -ArgumentList "$cylrArgs"
+Write-Host " ########### END CYLR ###########`n"
+Write-Host "Collection Complete!`n"
